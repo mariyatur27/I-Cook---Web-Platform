fb737675c5c5259678fe013ea51de08a757235e7
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2022-01-07 11:41:34 -0500

    Update fetch_data.js

--- aviato/aviato/themes/aviato/js/fetch_data.js
+++ aviato/aviato/themes/aviato/js/fetch_data.js
@@ -1,15 +1,40 @@
-// Record if we've already fetched data
-var dataFetched = false;
-var recipes = [];
-
-async function fetch_data() {
-    if (window.location.protocol == "file:") {
-        // For security reasons, scripts can't load files over the file: protocol
-        // Just mark data as fetched, and return.
-        console.log("Running from a file, won't attempt to get data from .json files");
-        dataFetched = true;
-        return;
+var recepies = [
+    {
+        "name": "Cinnamon Buns",
+        "id": "cinnamonbuns",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente sequi magni ad omnis, maxime impedit quia, ea illum vitae earum reprehenderit dolor dolorem beatae exercitationem tempore nisi aperiam quis. ",
+        "preperation_time" : "2 hours",
+        "servings": "6",
+        "introduction" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente sequi magni ad omnis, maxime impedit quia, ea illum vitae earum reprehenderit dolor dolorem beatae exercitationem tempore nisi aperiam quis. Provident?",
+        "ingredients" : [
+            {"sugar" : "60g"},
+            {"milk" : "100g"},
+            {"flour" : "2 cups"},
+            {"cinnamon" : "30g"},
+            {"butter" : "1 cup"}
+        ],
+        "directions" : [
+            {"step_1" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente sequi magni ad omnis, maxime impedit quia, ea illum vitae earum reprehenderit dolor dolorem beatae exercitationem tempore nisi aperiam quis. "},
+            {"step_2" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente sequi magni ad omnis, maxime impedit quia, ea illum vitae earum reprehenderit dolor dolorem beatae exercitationem tempore nisi aperiam quis. "},
+            {"step_3" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente sequi magni ad omnis, maxime impedit quia, ea illum vitae earum reprehenderit dolor dolorem beatae exercitationem tempore nisi aperiam quis. "},
+            {"step_4" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente sequi magni ad omnis, maxime impedit quia, ea illum vitae earum reprehenderit dolor dolorem beatae exercitationem tempore nisi aperiam quis. "},
+        ]
     }
-    // Get recipes from js/recipes.json
-    recipes = await (await fetch("js/recipes.json")).json();
+];
+
+
+var dataFetched = false;
+
+async function fetchData() {
+    if (window.location.protocol == "file:") {
+        console.log("fetchData: Running from a file, won't attempt to load data.");
+        dataFetched = true;
+    } else {
+        console.log("fetchData: Starting to load data.");
+        recepies = await (await fetch("js/recepies.json")).json();
+    }
+    console.log("fetchData: Starting to sort data.");
+    events.sort((event1, event2) => {return event1.start_time - event2.start_time});
+    console.log("fetchData: Done sorting data.");
+    dataFetched = true;
 }
