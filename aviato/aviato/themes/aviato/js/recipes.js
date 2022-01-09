5b37bab80421c9a46159411328ae466293c6fbd9
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2022-01-08 19:04:57 -0500

    new changes

--- aviato/aviato/themes/aviato/js/recipes.js
+++ aviato/aviato/themes/aviato/js/recipes.js
@@ -12,32 +12,35 @@
 <script>build_recipe_page(new URL(window.location.href).searchParams.get("id"));</script>
 */
 
-async function build_recipe_page(id=null) {
+async function build_recipe_page(divID, id=null) {
     if (!dataFetched) {
         await fetch_data();
     }
+
+    let section = document.getElementById("recipes");
     // If this page is being served from an actual website, the recipes variable will now have the
     // same contents as recipes.json. So, you can do something like: 
-    /*
+    
     for (const recipe of recipes) {
         if (id == null || recipe.id == id) {
-            // build page
+            // Build the page
+           let recipe_name = document.createElement("h4"); recipe_name.classList.add("r_name"); recipe_name.innerText = recipe.name;
+           section.appendChild(recipe_name); 
         }
     }
-    */
-   // Or, if you only want to display 1 recipe per page:
-   /*
-   let found = false;
-   for (var recipe of recipes) {
-       if (recipe.id == id) {
-           found = true;
-           break;
-       }
-   }
-   if (!found) {
-       // Display something to indicate that a matching recipe could not be found
-       return;
-   }
+
+// Or, if you only want to display 1 recipe per page:
+   
+//    let found = false;
+//    for (var recipe of recipes) {
+//        if (recipe.id == id) {
+//            found = true;
+//            break;
+//        }
+//    }
+//    if (!found) {
+//        // Display something to indicate that a matching recipe could not be found
+//        return;
+//    }
    // Display the recipe
-   */
 }
